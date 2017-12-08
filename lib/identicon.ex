ca47dac399198913a6aeca73abb0a0ee66b16ed4
Vyzaldy Sanchez <v.sanchez@gbh.com.do>
2017-12-08 17:10:46 -0400

    Picks color for image

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,9 +1,18 @@
 defmodule Identicon do
     def generate_from(string) do
-        hash_input(string)
+        hash_input(string) 
+        |> pick_color
     end
 
     def hash_input(input) do
-        :crypto.hash(:md5, input) |> :binary.bin_to_list
+        hex = :crypto.hash(:md5, input) |> :binary.bin_to_list
+
+        %Identicon.Image{hex: hex}
+    end
+
+    def pick_color(image) do
+        %Identicon.Image{hex: [red, green, blue | _]} = image
+
+        [red, green, blue]
     end
 end
