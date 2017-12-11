20aca7d11e3a3eda202667c90f92cd62ffb0b755
Vyzaldy Andres Sanchez <vyzaldysanchez@gmail.com>
2017-12-11 00:52:02 -0400

    Mirror rows for grid

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,6 +2,7 @@ defmodule Identicon do
     def generate_from(string) do
         hash_input(string) 
         |> pick_color
+        |> build_grid
     end
 
     def hash_input(input) do
@@ -13,6 +14,18 @@ defmodule Identicon do
     def pick_color(image) do
         %Identicon.Image{hex: [red, green, blue | _]} = image
 
-        [red, green, blue]
+        %Identicon.Image{image | color: {red, green, blue}}  
+    end
+
+    def build_grid(image) do
+        image.hex
+        |> Enum.chunk(3)
+        |> Enum.map(&mirror_row/1)
+    end
+
+    defp mirror_row(hex_row) do
+        [first, second, _] = hex_row
+
+        hex_row ++ [ second, first ]
     end
 end
